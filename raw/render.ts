14f0fdc20341ca09d7c5a3e9e02e89e4d5260a91
Jonathan Richards <jonathan.richards@ecs.co.uk>
2024-07-01 15:29:03 +0100

    split up render function

--- raw/render.ts
+++ raw/render.ts
@@ -1,40 +1,23 @@
 import { marked } from "marked";
 import prettier from "prettier";
 
-const about = {
-  title: "Jon Richards - Senior Developer / Tech Lead",
-  description: "TODO",
-  favicon: "assets/favicon.svg",
-};
+import template from "./template.ts";
 
 const input = ".";
 const output = "../html";
 
-const markdown = await Deno.readTextFile(`${input}/cv.md`);
-const body = marked.parse(markdown);
-
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>${about.title}</title>
-
-  <meta name="viewport" content="width=device-width,initial-scale=1"/>
-  <meta name="description" content="${about.description}"/>
-
-  <link rel="stylesheet" href="assets/reset.css">
-  <link rel="icon" href="${about.favicon}">
-</head>
-<body>
-${body}
-</body>
-</html>
-`;
-
-const formatted = await prettier.format(html, { parser: "html" });
-
 await Deno.mkdir(`${output}/assets`, { recursive: true });
+
+const markdown = await Deno.readTextFile(`${input}/cv.md`);
+
+const about = {
+  title: "Jon Richards - Senior Developer / Tech Lead",
+  description: "TODO",
+  favicon: "assets/favicon.svg",
+  body: await marked.parse(markdown),
+};
+
+const formatted = await prettier.format(template(about), { parser: "html" });
 
 // todo need better way to deal with all the css
 for await (const stylesheet of Deno.readDir(`${input}/assets`)) {
@@ -44,4 +27,4 @@ for await (const stylesheet of Deno.readDir(`${input}/assets`)) {
   );
 }
 
-Deno.writeTextFile(`${output}/index.html`, formatted);
+await Deno.writeTextFile(`${output}/index.html`, formatted);
