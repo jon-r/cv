6b04fc9a69c685c326b35d547a54453dcca245bd
Jonathan Richards <jonathan.richards@ecs.co.uk>
2024-07-11 16:17:57 +0100

    parent manages the css now

--- raw/template.ts
+++ raw/template.ts
@@ -9,91 +9,82 @@ type AboutProps = {
 function template(about: AboutProps) {
   return `<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8"/>
+  <head>
+    <meta charset="UTF-8" />
     <title>${about.title}</title>
 
-    <meta name="viewport" content="width=device-width,initial-scale=1"/>
-    <meta name="description" content="${about.description}"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="description" content="${about.description}" />
 
-    <link rel="icon" href="${about.favicon}"/>
+    <link rel="icon" href="${about.favicon}" />
 
-    <link rel="stylesheet" href="assets/reset.css"/>
-    <link rel="stylesheet" href="assets/styles.css"/>
-    <link id="style_main" rel="stylesheet" href="assets/style-main.css"/>
-</head>
-<body>
-<label class="style-select">
-    Style:
-    <select id="select_style">
-        <option value="main">Main</option>
-        <option value="red">Red</option>
-    </select>
-</label>
-<div class="page">${about.body}</div>
-</body>
+    <link rel="stylesheet" href="assets/reset.css" />
+    <link rel="stylesheet" href="assets/styles.css" />
+    <link id="style_main" rel="stylesheet" href="assets/style-main.css" />
+  </head>
+  <body>
+    ${about.body}
+  </body>
 
-<script>
-    const stylesheets = {
-        // main: document.querySelector("#style_main"),
-        // red: document.querySelector("#style_red"),
-    };
-
-    //  todo look at a way to not load these until enabled?
-    const select = document.getElementById("select_style");
+  <script>
+    const stylesheets = {};
 
     function appendStylesheet(name) {
-        const existingCss = document.querySelector(\`#style_{{name}\`);
+      const existingCss = document.querySelector(\`#style_\${name}\`);
 
-        if (existingCss) {
-            return existingCss
-        }
+      if (existingCss) {
+        return existingCss;
+      }
 
-        const newCss = document.createElement('link');
-        newCss.setAttribute('rel', 'stylesheet');
-        newCss.setAttribute('href', \`assets/style-{{name}.css\`);
-        newCss.setAttribute('id', \`style_\{{name}\`);
-        document.head.appendChild(newCss);
+      const newCss = document.createElement("link");
+      newCss.setAttribute("rel", "stylesheet");
+      newCss.setAttribute("href", \`assets/style-\${name}.css\`);
+      newCss.setAttribute("id", \`style_\${name}\`);
+      document.head.appendChild(newCss);
 
-        return newCss;
+      return newCss;
     }
 
     function enableStylesheet(name) {
-        Object.entries(stylesheets).forEach(([key, value]) => {
-            if (name === key) {
-                value.removeAttribute("disabled");
-            } else {
-                value.setAttribute("disabled", true);
-            }
-        });
+      Object.entries(stylesheets).forEach(([key, value]) => {
+        if (name === key) {
+          value.removeAttribute("disabled");
+        } else {
+          value.setAttribute("disabled", true);
+        }
+      });
     }
 
     function updateStyles(styleName) {
-        stylesheets[styleName] = appendStylesheet(styleName)
-        enableStylesheet(styleName);
+      stylesheets[styleName] = appendStylesheet(styleName);
+      enableStylesheet(styleName);
     }
 
+    window.addEventListener("message", (e) => {
+      // todo this needs some security?
+      updateStyles(e.data);
+    });
 
-    select.addEventListener("change", (e) => updateStyles(e.target.value));
-    
     function addSomeJazz(el) {
-      if (!el.classList.contains('jazzy')) {
+      if (!el.classList.contains("jazzy")) {
         el.classList.add("jazzy");
         [...Array(6)].forEach(() => {
-          const particle = document.createElement('i');
-          particle.className = 'particle';
-          console.log(particle);
-          el.appendChild(particle)
-        })
+          const particle = document.createElement("i");
+          particle.className = "particle";
+          el.appendChild(particle);
+        });
       }
     }
-    
-    document.querySelectorAll('h2').forEach(el => el.addEventListener("mouseenter", () => addSomeJazz(el)))
 
-    updateStyles('main')
-</script>
-</html>`
-      .replaceAll('{{', "${"); // todo is there a better way to do this? nested string literals
+    document
+      .querySelectorAll("h2")
+      .forEach((el) =>
+        el.addEventListener("mouseenter", () => addSomeJazz(el)),
+      );
+
+    updateStyles("main");
+  </script>
+</html>`;
 }
 
 export default template;
