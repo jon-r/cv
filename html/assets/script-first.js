e5a9c3bdcd50b0322284e287de3967d887d873e0
Jon Richards <jonr.web@gmail.com>
2024-09-16 14:46:03 +0100

    script to enable the darmode automatically

---
+++ html/assets/script-first.js
@@ -0,0 +1,8 @@
+// todo and not print
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+    document.body.classList.add("dark-theme");
+} else {
+    document.body.classList.remove("dark-theme");
+}
